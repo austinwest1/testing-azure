1873a1313dc0b64509672c0792c86c05cf16c728
austinwest1 <austinwest1@mail.weber.edu>
2021-01-21 23:08:45 -0700

    fixed stuff

--- scripts/rps_game3.js
+++ scripts/rps_game3.js
@@ -1,19 +1,11 @@
 "use strict";
 
-function rpsGame3()
-{
-    // midCol = "<h1>Wins: " + game2Wins + "</h1>";
-    // midCol += "<h1>CPU Wins: " + game2CPUwins + "</h1><br />"
-    midCol =  "<div id='col2-1'>hi</div>";
+function rpsGame3() {
+
+    midCol = "<div id='col2-1'>hi</div>";
 
     //displays number of wins
 
-
-    // var CPUThrows = ["rockButton", "rockButton", "rockButton", 
-    //                 "paperButton", "paperButton", "paperButton", 
-    //                 "scissorsButton", "scissorsButton", "scissorsButton"];
-
-    // var CPUGuess = Math.floor(Math.random() * 9);
 
     var cpuChoice = getCpuChoice();
     checkCPUGuess();
@@ -21,42 +13,34 @@ function rpsGame3()
     midCol += cpuChoice;
     document.getElementById("col2").innerHTML = midCol;
 
-    if (cpuChoice === "rockButton")
-    {
+    if (cpuChoice === "rockButton") {
         styles = document.createElement("style");
         document.head.appendChild(styles);
-        document.styleSheets[document.styleSheets.length-1].insertRule(
-             "input#cpuRockButton { \
+        document.styleSheets[document.styleSheets.length - 1].insertRule(
+            "input#cpuRockButton { \
                 border: 2px solid rgba(255, 0, 0, 0.7); \
              }", 0);
     }
-    else if (cpuChoice === "paperButton")
-    {
+    else if (cpuChoice === "paperButton") {
         styles = document.createElement("style");
         document.head.appendChild(styles);
-        document.styleSheets[document.styleSheets.length-1].insertRule(
-             "input#cpuPaperButton { \
+        document.styleSheets[document.styleSheets.length - 1].insertRule(
+            "input#cpuPaperButton { \
                 border: 2px solid rgba(255, 0, 0, 0.7); \
              }", 0);
     }
-    else if (cpuChoice === "scissorsButton")
-    {
+    else if (cpuChoice === "scissorsButton") {
         styles = document.createElement("style");
         document.head.appendChild(styles);
-        document.styleSheets[document.styleSheets.length-1].insertRule(
-             "input#cpuScissorsButton { \
+        document.styleSheets[document.styleSheets.length - 1].insertRule(
+            "input#cpuScissorsButton { \
                 border: 2px solid rgba(255, 0, 0, 0.7); \
              }", 0);
     }
 
     var choices = document.getElementsByClassName("userButtons3");
-    for (var i = 0; choices.length; i++)
-    {
+    for (var i = 0; choices.length; i++) {
         choices[i].onclick = getResults;
     }
-    
-    // for (var i = 0; i < 10; i++)
-    // {
 
-    // }
 }
